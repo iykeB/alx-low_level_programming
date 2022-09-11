07837895368c77dab4f1d8ecf277f2cd6e788ccf
Bright <simeonbright11@gmail.com>
2022-09-11 13:24:56 -0700

    my file commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - prints exactly with proper grammar,
  * but the outcome is a piece of art,followed by a new line.
- * Return 0 if exited properly, non zero otherwise
+ * Return: 0 if exited properly, non zero otherwise
  */
 int main(void)
 {
