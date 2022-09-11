1920b0e53d633ed67a11ed418ef92c17aab2bb97
Bright <simeonbright11@gmail.com>
2022-09-11 12:46:24 -0700

    my commit for 5 print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Aprogram that prints exactly with proper grammar,
- * but the outcome is a piece of art,,
- * followed by a new line.
+ * main - prints exactly with proper grammar,
+ * but the outcome is a piece of art,followed by a new line.
+ * Return 0 if exited properly, non zero otherwise
  */
 int main(void)
 {
