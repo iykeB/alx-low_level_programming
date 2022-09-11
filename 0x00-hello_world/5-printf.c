dca8b6fb89223958566c9eebef17d057a14b5fc7
Bright <simeonbright11@gmail.com>
2022-09-11 10:24:42 -0700

    my c code commi for prinft

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Aprogram that prints exactly with proper grammar, 
+ * but the outcome is a piece of art,, 
+ * followed by a new line.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
