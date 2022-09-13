31ea76d5833db0f64a1f3b4e45aca5257c210faa
Bright <simeonbright11@gmail.com>
2022-09-13 10:46:42 -0700

    my commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/**main - prints the alphabet in lowercase,
+/**
+ * main - prints the alphabet in lowercase,
  * followed by a new line, except q and e
  * Return: Aways 0 (Sucess)
  */
