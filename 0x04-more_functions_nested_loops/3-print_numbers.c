06a75ad2d0d5a01c81427318210e924ef35152b0
Bright <simeonbright11@gmail.com>
2022-09-18 08:37:43 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numbers
+ * New line: to be printed
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	while (n <= '9')
+	{
+		putchar(n);
+	}
+	printf("\n");
+}
+
