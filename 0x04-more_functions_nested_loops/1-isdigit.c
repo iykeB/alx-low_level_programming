f3647ec47ce4d83ca5412516990fc5ae840252dd
Bright <simeonbright11@gmail.com>
2022-09-18 05:57:21 -0700

    my commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,9 +3,9 @@
 /**
  * _isdigit - checks for a digit
  * @c: is the  digit
- * Return 1 if c is a digit 0 otherwise
+ * Return: 1 if c is a digit 0 otherwise
  */
 int _isdigit(int c)
 {
-	Return(c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
