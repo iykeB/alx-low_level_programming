363d9eb9fff2d00e5ea4c40fe3524ee0f61b670b
Bright <simeonbright11@gmail.com>
2022-09-18 08:06:28 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - multiply two numbers
+ *@a: and
+ *@b: numbers to be mutiply
+ * return: void
+ */
+void int mul(int a, int b)
+{
+	/* mul var */
+	int mul;
+
+	mul = (a * b);
+	return (mul);
+}
